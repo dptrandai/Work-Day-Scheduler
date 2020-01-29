df4c911b6976e3d161f8724eb88d2e695eb9e4d5
Dustin Trandai <dptrandai@gmail.com>
2020-01-29 14:38:59 -0600

    edit html and started Js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Variable for the input field
+var descriptionInput = document.getElementById('jobDescripton');
+var saveBtn = document.getElementById('saveBtn');
+
+//click event, when the user clicks the save button it should append to input field
+$('#saveBtn').on('click', function(){
+    
+})
